489a9fc7053d46dc8777bdf09468cd871a0597f7
Praveen Subramanian <Praveentechnic@gmail.com>
2021-03-27 20:39:41 +0530

    fixed bug of sub directories

--- RFSClient.py
+++ RFSClient.py
@@ -7,6 +7,7 @@ import shutil
 import trace
 import traceback
 
+
 def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
@@ -15,6 +16,31 @@ def zipdir(path, ziph):
                        os.path.relpath(os.path.join(root, file), 
                                        os.path.join(path, '..')))
 
+def copytree(src, dst, symlinks=False):
+    names = os.listdir(src)
+    if(not os.path.exists(dst)):
+        os.makedirs(dst)
+    errors = []
+    for name in names:
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks)
+            else:
+                shutil.move(srcname, dstname)
+            # XXX What about devices, sockets etc.?
+        except OSError as why:
+            errors.append((srcname, dstname, str(why)))
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except Exception as err:
+            errors.extend(err.args[0])
+    if errors:
+        raise Exception(errors)
 
 class UploadFile():
     def __init__(self, targetFileorFolder , UploadAs  ):
@@ -61,10 +87,10 @@ class DownloadFile():
     def unzip_file(self):
         with zipfile.ZipFile(self.FiletoDownload, 'r') as zip_ref:
             zip_ref.extractall(self.id)
-            for f in os.listdir(self.id+'/'+os.listdir(self.id)[0]):
-                shutil.move(os.path.join(self.id+'/'+os.listdir(self.id)[0], f), os.path.join(self.Destination, f) ) #BUG: while having sub directories , this is failing 
-            os.rmdir(self.id+'/'+os.listdir(self.id)[0])
-            ps.rmdir(self.id)
+        copytree(self.id+'/'+os.listdir(self.id)[0], self.Destination)    
+        shutil.rmtree(self.id)
+        
+        
             
 
     def download(self):
